818b30b65cd8a3c5c8bfdadbff359e7845fcabda
02380 <49149558+02380@users.noreply.github.com>
2020-07-15 22:26:29 +0100

    more edits

--- rollup.config.js
+++ rollup.config.js
@@ -22,7 +22,8 @@ export default {
 			// a separate file - better for performance
 			css: css => {
 				css.write('public/build/bundle.css');
-			}
+			},
+      		preprocess: autoPreprocess()
 		}),
 
 		// If you have external dependencies installed from
